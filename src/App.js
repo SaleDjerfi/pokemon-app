0e97a276b7ebbcdaa1ae9555477556b120c3bde7
SaleDjerfi <69083655+SaleDjerfi@users.noreply.github.com>
2021-08-17 16:46:24 +0200

    Fetch with API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect} from 'react'
 import PokemonList from './Components/PokemonList';
+import axios from 'axios'
 
 function App() {
-  const [pokemon, setPokemon] = useState(["charmander", "bulbasaur"])
+  const [pokemon, setPokemon] = useState([])
+
+  useEffect(() => {
+    axios.get("https://pokeapi.co/api/v2/pokemon").then(res => {
+    setPokemon(res.data.results.map(p => p.name))
+  })
+  }, [])
+
+  
 
   return (
     <PokemonList pokemon={pokemon}/>
